9161ed7681379fc0a93ffc24524d4a011799ef44
MgHtetMyatAung <htetmyataung16000@gmail.com>
2025-09-22 10:10:55 +0630

    fix: fixes image ratio and heading sequent

--- src/components/pages/home/hero-section.tsx
+++ src/components/pages/home/hero-section.tsx
@@ -9,13 +9,13 @@ export default function HeroSection() {
       <div className=" max-w-[1200px] mx-auto h-auto lg:h-[600px] flex flex-col lg:flex-row">
         <div className=" text-gray-50 lg:grow h-full lg:flex lg:items-center py-12 lg:py-0">
           <div className=" space-y-6 text-center lg:text-start">
-            <h5 className=" text-gray-200 text-2xl lg:text-3xl">Pro.Beyond.</h5>
-            <h2 className=" text-7xl lg:text-8xl font-light">
+            <p className=" text-gray-200 text-2xl lg:text-3xl">Pro.Beyond.</p>
+            <h1 className=" text-7xl lg:text-8xl font-light">
               IPhone 14 <span className=" font-medium">Pro</span>
-            </h2>
-            <h6 className=" text-gray-200 text-xl lg:text-2xl">
+            </h1>
+            <p className=" text-gray-200 text-xl lg:text-2xl">
               Created to change everything for the better. For everyone
-            </h6>
+            </p>
             <Link href={"/products"}>
               <Button variant="outlined">Shop Now</Button>
             </Link>
@@ -25,16 +25,16 @@ export default function HeroSection() {
           <Image
             src={"/imgs/hero-mock-phone.png"}
             alt="mock"
-            width={400}
-            height={600}
+            width={406}
+            height={632}
             className=" h-full w-auto hidden lg:block"
             priority
           />
           <Image
             src={"/imgs/hero-mock-phone-sm.png"}
             alt="mock"
-            width={340}
-            height={280}
+            width={343}
+            height={289}
             className=" w-full sm:w-[400px] h-auto mx-auto block lg:hidden"
             priority
           />
